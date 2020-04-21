628d7c7d586069c365c73f4a4f9a303555963aaa
Olga_Sanni <dev.sannikov@gmail.com>
2020-04-21 19:01:45 -0400

    SearchMedicalDictionary, check if exists

--- src/components/SerachForMedicalDictionary.js
+++ src/components/SerachForMedicalDictionary.js
@@ -13,8 +13,8 @@ class SearchForMedicalDictionary extends React.Component{
         const key=process.env.REACT_APP_MEDICAL_API_KEY;
         try{
         let getWord=await axios.get(`https://dictionaryapi.com/api/v3/references/medical/json/${this.props.id}?key=${key}`)
-        this.setState({wordInfo: getWord});
-        console.log(this.state.wordInfo.data[0]);
+        this.setState({wordInfo: getWord.data[0]});
+        console.log(this.state.wordInfo.shortdef);
     }
         catch(error){
             console.log(error);
@@ -23,10 +23,22 @@ class SearchForMedicalDictionary extends React.Component{
     componentDidMount(){
         this.getWordInfo();
     }
-    
+    checkIfExist(){
+        if (this.state.wordInfo===""){
+            return <p>Word information not found</p>
+        }
+        else{
+            let info=this.state.wordInfo.shortdef.map((res,index)=>{
+                return <p>{index+1}. {res}</p>})
+            return <div>
+                    <p>{this.state.wordInfo</p>
+                 {info}
+                  </div>
+        }
+    }
     render(){
         return(<div>
-            Hello
+       {this.checkIfExist()}
         </div>)
     }
 }
