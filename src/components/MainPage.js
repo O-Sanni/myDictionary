2ae49a07c40ac6f011425b2d696436e9db6149a3
Olga_Sanni <dev.sannikov@gmail.com>
2020-04-21 15:52:10 -0400

    MainPage.js

---
+++ src/components/MainPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+class MainPage extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Welcome ro myDictionary</h1>
+            </div>
+        )
+    }
+}
