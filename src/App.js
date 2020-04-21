7a6df4855936c34c30aa79b882c065893eb100c7
Olga_Sanni <dev.sannikov@gmail.com>
2020-04-21 19:30:16 -0400

    SearchForRegular working API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import SearchForMedicalDictionary from "./components/SerachForMedicalDictionary";
+import SearchForRegular from "./components/SearchForRegular"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <SearchForMedicalDictionary id="helicobacter" />
+     {/* <SearchForMedicalDictionary id="helicobacter" /> */}
+     <SearchForRegular id="obliquity"/>
     </div>
   );
 }
---
+++ src/components/SearchForRegular.js
@@ -0,0 +1,50 @@
+import React from "react";
+import axios from "axios";
+
+
+class SearchForRegular extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            wordInfo: ""
+        }
+    }
+    async getWordInfo(){
+        const key=process.env.REACT_APP_API_KEY_THESAURUS;
+        try{
+        let getWord=await axios.get(`https://dictionaryapi.com/api/v3/references/thesaurus/json/${this.props.id}?key=${key}`)
+        this.setState({wordInfo: getWord.data[0]});
+        console.log(this.state.wordInfo);
+    }
+        catch(error){
+            console.log(error);
+        }
+    }
+    componentDidMount(){
+        this.getWordInfo();
+    }
+    checkIfExist(){
+        if (this.state.wordInfo===""){
+            return <p>Word information not found</p>
+        }
+        else{
+            // let info=this.state.wordInfo.shortdef.map((res,index)=>{
+            //     return <p>{index+1}. {res}</p>})
+            // return <div>
+            //         <p>{this.state.wordInfo.meta.id.toUpperCase()}</p>
+            //         <p>Pronunciation: [{this.state.wordInfo.hwi.prs[0].mw}]</p>
+            //         <p>Gramatical Function: {this.state.wordInfo.fl}</p>
+            //         <p>Definition(s):</p>
+            //         <div> {info}</div>
+                   
+            //       </div>
+        }
+    }
+    render(){
+        return(<div>
+       {this.checkIfExist()}
+        </div>)
+    }
+}
+
+export default SearchForRegular;
