c4c3d9deb64ecfad9661778a16bd4e98d08f037b
Olga_Sanni <dev.sannikov@gmail.com>
2020-04-22 12:25:25 -0400

    add Translation to Navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,7 +2,9 @@ import React from "react";
 import {Route, Switch, Link , BrowserRouter as Router} from "react-router-dom";
 import MainPage from "./MainPage"
 import MedicalDictionary from "./MedicalDictionary"
+import YranslationPage from "./TranslationPage"
 import RegularDictionary from "./RegularDictionary"
+import TranslationPage from "./TranslationPage";
 
 function Navigation(){
     return (
@@ -18,12 +20,16 @@ function Navigation(){
                     <li>
                         <Link to="/thesaurus">Thesaurus</Link>
                     </li>
+                    <li>
+                        <Link to="/translation">Translation</Link>
+                    </li>
                 </ul>
             </nav>
             <Switch>
                 <Route exact path="/" component={MainPage}></Route>
                 <Route exact path="/medical_dictionary" component={MedicalDictionary}></Route>
                 <Route exact path="/thesaurus" component={RegularDictionary}></Route>
+                <Route exact path="/translation" cmponent={TranslationPage} ></Route>
             </Switch>
         </Router>
     )
