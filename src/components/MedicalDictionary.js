142bf7e125b9173ed48b2a34626525c79aa0cb84
Olga_Sanni <dev.sannikov@gmail.com>
2020-04-24 17:37:35 -0400

    make everything pretty

--- src/components/MedicalDictionary.js
+++ src/components/MedicalDictionary.js
@@ -16,12 +16,14 @@ class MedicalDictionary extends React.Component{
       //bind my button and input handler in order to use it and manipulate the state
       this.getWordHandler=this.getWordHandler.bind(this);
       this.submitButton=this.submitButton.bind(this);
-  }  
+  } 
+
   //wordHandler will update the state of the searchWord , the word which user want to search
   getWordHandler(event){
       event.preventDefault();
       this.setState({searchWord: event.target.value})
   }
+
   //submitButton function will setState 
   submitButton(event){
     event.preventDefault();// prevent restart of the page 
@@ -30,27 +32,27 @@ class MedicalDictionary extends React.Component{
 
    render(){
         return (
-        <div id="main-div-medical"> 
-        <div className="form-button-dictionaries">
-        <form className="form-dictionaries" onSubmit={this.submitButton} >
-        <p className="p-form-dictionaries">Please enter word and then press submit</p>
-        {/* <input /> will have the event  value, such as word that user want to search*/}
-        <input className="input-search-dictionaries" type="text" value={this.state.searchWord} onChange={this.getWordHandler} placeholder="Enter word" />
-        <div className="button-clear-dictionaries">
-        <input className="submit-button-dictionaries" type="submit" />
-        {/* clear button will clear the state, put values to initial state in order to do a new search */}
-        <button className="clear-button-dictionaries" type="button" onClick={()=>{this.setState({searchWord: initialState, search:false})}}>Clear</button>
-        <p className="clear-text">If you need to start new search please press clear</p>
-        </div>
-        </form>
-        </div>
-        <div className="search-result-dictionaries">
-        {/* by clicking submit button we are changing the state of search , when search is true we are calling 
-        <SearchForMedicalDictionary /> and send the id as the word which user want to look for, 
-        if the search is false just return the phrase */}
-       {this.state.search ? (<SearchForMedicalDictionary id={this.state.searchWord.toLowerCase()} />) : ""}
-        </div>
-        </div>)
+                <div id="main-div-medical"> 
+                  <div className="form-button-dictionaries">
+                    <form className="form-dictionaries" onSubmit={this.submitButton} >
+                      <p className="p-form-dictionaries">Please enter word and then press submit</p>
+                      {/* <input /> will have the event  value, such as word that user want to search*/}
+                      <input className="input-search-dictionaries" type="text" value={this.state.searchWord} onChange={this.getWordHandler} placeholder="Enter word" />
+                      <div className="button-clear-dictionaries">
+                        <input className="submit-button-dictionaries" type="submit" />
+                        {/* clear button will clear the state, put values to initial state in order to do a new search */}
+                        <button className="clear-button-dictionaries" type="button" onClick={()=>{this.setState({searchWord: initialState, search:false})}}>Clear</button>
+                        <p className="clear-text">If you need to start new search please press clear</p>
+                      </div>
+                    </form>
+                  </div>
+                  <div className="search-result-dictionaries">
+                    {/* by clicking submit button we are changing the state of search , when search is true we are calling 
+                    <SearchForMedicalDictionary /> and send the id as the word which user want to look for, 
+                    if the search is false just return the phrase */}
+                    {this.state.search ? (<SearchForMedicalDictionary id={this.state.searchWord.toLowerCase()} />) : ""}
+                  </div>
+                </div>)
    }  
 }
 
