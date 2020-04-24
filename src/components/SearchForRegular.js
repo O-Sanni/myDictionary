8379c38dda25780742d16fa90710541a7ef5f66e
Olga_Sanni <dev.sannikov@gmail.com>
2020-04-24 17:42:05 -0400

    make everything pretty

--- src/components/SearchForRegular.js
+++ src/components/SearchForRegular.js
@@ -9,6 +9,7 @@ class SearchForRegular extends React.Component{
             wordInfo: ""
         }
     }
+
     async getWordInfo(){
         const key=process.env.REACT_APP_API_KEY_THESAURUS;
         try{
@@ -20,6 +21,7 @@ class SearchForRegular extends React.Component{
             console.log(error);
         }
     }
+
     componentDidMount(){
         this.getWordInfo();
     }
@@ -34,33 +36,37 @@ class SearchForRegular extends React.Component{
         else{
             let gramFunction=this.state.wordInfo.fl;
             gramFunction=gramFunction[0].toUpperCase()+gramFunction.slice(1);
-            let definition=this.state.wordInfo.shortdef.map((res,index)=>{
-                return <p className="class-definitions-regular" id={"difitition-regular-id-"+index}>{index+1}. {res}</p>})
-            let synonyms=this.state.wordInfo.meta.syns.map((res,index)=>{  
+            let definition=this.state.wordInfo.shortdef.map((res,index)=>
+            {
+                return <p className="class-definitions-regular" id={"difitition-regular-id-"+index}>{index+1}. {res}</p>
+            });
+            let synonyms=this.state.wordInfo.meta.syns.map((res,index)=>
+            {  
                 return ( <p className="p-synonyms-class" id={"div-synonyms-"+index+30}>{index+1}. {res.join(", ")}</p> )
             });
-            let antonyms=this.state.wordInfo.meta.ants.map((res,index)=>{
+            let antonyms=this.state.wordInfo.meta.ants.map((res,index)=>
+            {
                 return ( <p className="p-antonyms-class" id={"p-antonyms"+index}>{index+1}. {res.join(", ")}</p> )
             });
 
             return <div className="info-div-dictionaries">
-               <p id="p-word-name-regular">{this.state.wordInfo.meta.id.toUpperCase()}</p>
-                    <p id="p-gram-func-regular">Gramatical Function: {gramFunction}</p>
-                     <h3 id="h3-def-regular">Definition(s):</h3>
-                    <div id="div-def-regular"> {definition}</div>
-                    <h3 id="h3-synonyms-regular">Synonyms:</h3>
-                    {synonyms}
-                    <h3 id="h3-antonyms-regular">Antonyms:</h3>
-                    {antonyms}
-                     
-             </div>
+                        <p id="p-word-name-regular">{this.state.wordInfo.meta.id.toUpperCase()}</p>
+                        <p id="p-gram-func-regular">Gramatical Function: {gramFunction}</p>
+                        <h3 id="h3-def-regular">Definition(s):</h3>
+                        <div id="div-def-regular"> {definition}</div>
+                        <h3 id="h3-synonyms-regular">Synonyms:</h3>
+                            {synonyms}
+                        <h3 id="h3-antonyms-regular">Antonyms:</h3>
+                            {antonyms}
+                    </div>
         }
     }
+
     render(){
         return(
-        <div className="main-div-dictionaries">
-       {this.checkIfExist()}
-        </div>)
+                <div className="main-div-dictionaries">
+                    {this.checkIfExist()}
+                </div>)
     }
 }
 
